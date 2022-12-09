95650b363614bda8277d823ff9a3dc50a06170a6
Barış <barisozcanli@gmail.com>
2022-12-09 21:06:18 +0100

    added deployment scripts

---
+++ k8s/deploy.sh
@@ -0,0 +1,14 @@
+docker build -t barisozcanli/multi-client:latest -t barisozcanli/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t barisozcanli/multi-server:latest -t barisozcanli/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t barisozcanli/multi-worker:latest -t barisozcanli/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push barisozcanli/multi-client:latest
+docker push barisozcnali/multi-server:latest
+docker push barisozcanli/multi-worker:latest
+
+docker push barisozcanli/multi-client:$SHA
+docker push barisozcnali/multi-server:$SHA
+docker push barisozcanli/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=barisozcanli/multi-server:$SHA
+kubectl set image deployments/client-deployment client=barisozcanli/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=barisozcanli/multi-worker:$SHA
